d1c9eddb8c04d310f17d0db06f1eec5d29e0d690
Edeanstuff <edeand1@gmail.com>
2019-04-21 21:58:21 -0700

    Adding text input and styles

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import Bootstrap from "bootstrap";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{padding: 10}}>
+      <Text style={title}>音楽さん</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="What song are you looking for?!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button>Search</Button>
       </View>
     );
   }
@@ -17,5 +28,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    borderBottom: '1px black solid',
+    color:'black',
+    fontSize:'1rem',
   },
 });
